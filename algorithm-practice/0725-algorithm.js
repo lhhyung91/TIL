3b457a23417f88c8d61c43b30d4f5be92cf687eb
lhhyung91 <lhhyung91@gmail.com>
2021-07-26 00:04:44 +0900

    알고리즘 문제 풀이

---
+++ algorithm-practice/0725-algorithm.js
@@ -0,0 +1,17 @@
+// 길이가 n이고, "수박수박수박수...."와 같은 패턴을 유지하는 문자열을 리턴하는 함수, solution을 완성하세요.
+// 예를들어 n이 4이면 "수박수박"을 리턴하고 3이라면 "수박수"를 리턴하면 됩니다.
+
+// 풀이
+function solution(n) {
+  var answer = "";
+  for (var i = 0; i < n; i++) {
+    if (i % 2 === 0) {
+      answer = answer + "수";
+    } else {
+      answer = answer + "박";
+    }
+  }
+  return answer;
+}
+
+solution();
