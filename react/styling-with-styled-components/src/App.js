d742e0523a9171c4466853bb5e2e74a6232bb3ab
lhhyung91 <lhhyung91@gmail.com>
2021-07-26 16:04:54 +0900

    styling-with-styled-components 실습

--- react/styling-with-styled-components/src/App.js
+++ react/styling-with-styled-components/src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useState } from "react";
+import styled, { ThemeProvider } from "styled-components";
 import Button from "./components/Button";
+import Dialog from "./components/Dialog";
 
 const Appblock = styled.div`
   width: 521px;
@@ -10,11 +11,55 @@ const Appblock = styled.div`
   padding: 1rem;
 `;
 
+const ButtonGroup = styled.div`
+  & + & {
+    margin-top: 1rem;
+  }
+`;
+
+const palette = {
+  blue: "#228be6",
+  gray: "#496057",
+  pink: "#f06595",
+};
+
 function App() {
+  const [dialog, setDialog] = useState(false);
+
+  const onClick = () => {
+    setDialog(true);
+  };
+
+  const onConfirm = () => {
+    setDialog(false);
+    console.log("확인");
+  };
+
+  const onCancel = () => {
+    setDialog(false);
+    console.log("취소");
+  };
+
   return (
-    <Appblock>
-      <Button>BUTTON</Button>
-    </Appblock>
+    <ThemeProvider theme={{ palette }}>
+      <>
+        <Appblock>
+          <Button color="pink" size="large" onClick={onClick}>
+            삭제
+          </Button>
+        </Appblock>
+        <Dialog
+          title="정말 삭제?"
+          confirmText="당연 삭제"
+          cancelText="아냐 잠깐만.."
+          visible={dialog}
+          onConfirm={onConfirm}
+          onCancel={onCancel}
+        >
+          데이터 진짜 삭제?
+        </Dialog>
+      </>
+    </ThemeProvider>
   );
 }
 
--- react/styling-with-styled-components/src/components/Button.js
+++ react/styling-with-styled-components/src/components/Button.js
@@ -1,5 +1,71 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { darken, lighten } from "polished";
+
+const colorStyles = css`
+  /* 색상 */
+  ${({ theme, color }) => {
+    const selected = theme.palette[color];
+    return css`
+      background: ${selected};
+      &:hover {
+        background: ${lighten(0.1, selected)};
+      }
+      &:active {
+        background: ${darken(0.1, selected)};
+      }
+      ${(props) =>
+        props.outline &&
+        css`
+          color: ${selected};
+          background: none;
+          border: 1px solid ${selected};
+          &:hover {
+            background: ${selected};
+            color: white;
+          }
+        `}
+    `;
+  }}
+`;
+
+const sizes = {
+  large: {
+    height: "3rem",
+    fontSize: "1.25rem",
+  },
+  medium: {
+    height: "2.25rem",
+    fontSiz: "1rem",
+  },
+  small: {
+    height: "1.75rem",
+    fontSize: "0.875rem",
+  },
+};
+
+const sizeStyles = css`
+  /* 크기 */
+  ${({ size }) => css`
+    height: ${sizes[size].height};
+    font-size: ${sizes[size].fontSize};
+  `}
+`;
+
+const fullWidthStyle = css`
+  ${(props) =>
+    props.fullWidth &&
+    css`
+      width: 100%;
+      justify-content: center;
+
+      &:not(:first-child) {
+        margin-left: 2rem;
+        margin-top: 1rem;
+      }
+}
+    `}
+`;
 
 const StyledButton = styled.button`
   /* 공통 스타일*/
@@ -15,25 +81,38 @@ const StyledButton = styled.button`
   padding-left: 1rem;
   padding-right: 1rem;
 
-  /* 크기 */
-  height: 2.25rem;
-  font-size: 1rem;
-
-  /* 색상 */
-  background: #228be6;
-  &:hover {
-    background: #339af0;
-  }
-  &:active {
-    background: #1c7ed6;
-  }
-
   /* 기타 */
   & + & {
     margin-left: 1rem;
   }
+
+  ${fullWidthStyle}
+  ${colorStyles}
+  ${sizeStyles}
 `;
 
-export default function Button({ children, ...rest }) {
-  return <StyledButton {...rest}>{children}</StyledButton>;
+export default function Button({
+  children,
+  color,
+  size,
+  outline,
+  fullWidth,
+  ...rest
+}) {
+  return (
+    <StyledButton
+      color={color}
+      size={size}
+      outline={outline}
+      fullWidth={fullWidth}
+      {...rest}
+    >
+      {children}
+    </StyledButton>
+  );
 }
+
+Button.defaultProps = {
+  color: "blue",
+  size: "medium",
+};
