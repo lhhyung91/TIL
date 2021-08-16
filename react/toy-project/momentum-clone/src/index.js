b55dec1fb8e6a0974a6ffe77b3ce1d2194e33be6
lhhyung91 <lhhyung91@gmail.com>
2021-08-16 23:50:21 +0900

    middleware 및 devtool 추가

--- react/toy-project/momentum-clone/src/index.js
+++ react/toy-project/momentum-clone/src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import rootReducer from "./modules";
 import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import logger from "redux-logger";
 
-const store = createStore(rootReducer);
-// console.log(store.getState());
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
 
 ReactDOM.render(
   <Provider store={store}>
@@ -19,7 +23,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
