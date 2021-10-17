39183d5ea4eb5c17b5fbc0daec0ca79e7c5fba3f
Hyo Hyung Lee <lhhyung91@gmail.com>
2021-10-17 22:15:27 +0900

    [Remove]

--- PAYHERE/src/styles/StyledSection.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const StyledSection = styled.section`
-  max-width: 100vw;
-  position: relative;
-  display: flex;
-  justify-content: center;
-  flex-flow: row;
-`;
