361bda7c2fca6f54da553aa50abb94438d8919dc
Hyo Hyung Lee <lhhyung91@gmail.com>
2021-10-18 22:15:31 +0900

    [practice]

---
+++ react/ts-react-tutorial/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Greetings from './Greetings';
+
+function App() {
+  const onClick = (name: string) => {
+    console.log(name);
+  };
+
+  return <Greetings name='리액트' onClick={onClick} />;
+}
+
+export default App;
