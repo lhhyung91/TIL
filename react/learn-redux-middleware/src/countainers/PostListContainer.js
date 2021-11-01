6a9fbc9bf24c46a9b44198f65f1a462edf851e8d
Hyo Hyung Lee <lhhyung91@gmail.com>
2021-11-01 23:58:20 +0900

    learn-redux-middleware 실습

--- react/learn-redux-middleware/src/countainers/PostListContainer.js
+++ react/learn-redux-middleware/src/countainers/PostListContainer.js
@@ -1,19 +1,20 @@
-import React from "react";
-import { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import PostList from "../components/PostList";
-import { clearPost, getPosts } from "../modules/posts";
+import React from 'react';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import PostList from '../components/PostList';
+import { clearPost, getPosts } from '../modules/posts';
 
-export default function PostListContainer() {
-  const { data, loading, error } = useSelector((state) => state.posts.posts);
+export default function PostListContainer({ postId }) {
+  const { data, loading, error } = useSelector(state => state.posts.posts);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getPosts());
+    if (data) return;
+    dispatch(getPosts(postId));
     return () => {
       dispatch(clearPost());
     };
-  }, [dispatch]);
+  }, [postId, dispatch]);
 
   if (loading && !data) return <div>로딩 중....</div>;
   if (error) return <div>에러 발생....</div>;
