af7e04b952d1f285030d2ff5e1b321764407fd7d
Hyo Hyung Lee <lhhyung91@gmail.com>
2021-11-10 23:53:49 +0900

    learn-ts-react-redux-tutorial

--- react/ts-react-redux-tutorial/src/components/GithubUsernameForm.tsx
+++ react/ts-react-redux-tutorial/src/components/GithubUsernameForm.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./GithubUsernameForm";
+import "./GithubUsernameForm.css";
 
 type GithubUsernameFormProps = {
   onSubmitUsername: (username: string) => void;
