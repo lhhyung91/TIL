de9a8ed33aaae7e4e82b872742bc84c5254b54a9
Hyo Hyung Lee <lhhyung91@gmail.com>
2021-11-10 23:43:13 +0900

    learn-ts-react-redux-tutorial

---
+++ react/ts-react-redux-tutorial/src/components/GithubUsernameForm.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import "./GithubUsernameForm";
+
+type GithubUsernameFormProps = {
+  onSubmitUsername: (username: string) => void;
+};
+
+export default function GithubUsernameForm({
+  onSubmitUsername,
+}: GithubUsernameFormProps) {
+  const [input, setInput] = useState("");
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onSubmitUsername(input);
+  };
+
+  const onCgange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
+  return (
+    <form onSubmit={onSubmit} className='GithubUsernameForm'>
+      <input
+        onChange={onCgange}
+        value={input}
+        placeholder='Github 계정을 입력하시오.'
+      />
+      <button>조회</button>
+    </form>
+  );
+}
