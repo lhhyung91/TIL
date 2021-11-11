ed8377954a0735b2532a0d5feebb5763f53fefd4
Hyo Hyung Lee <lhhyung91@gmail.com>
2021-11-11 18:49:12 +0900

    learn-ts-react-redux-tutorial

---
+++ react/ts-react-redux-tutorial/src/modules/github/thunks.ts
@@ -0,0 +1,23 @@
+// import { Dispatch } from 'redux';
+import { getUserProfile } from "../../api/github";
+import createAsyncThunk from "../../lib/createAsyncThunk";
+import { getUsersProfileAsync } from "./actions";
+
+// export function getUserProfileThunk(username: string) {
+//   return async (dispatch: Dispatch) => {
+//     const { request, success, failure } = getUsersProfileAsync;
+//     dispatch(request());
+//     try {
+//       const userProfile = await getUserProfile(username);
+//       dispatch(success(userProfile));
+//     } catch (e: any) {
+//       // e의 타입을 뭐로 해야 될 지 모르겠음
+//       dispatch(failure(e));
+//     }
+//   };
+// }
+
+export const getUserProfileThunk = createAsyncThunk(
+  getUsersProfileAsync,
+  getUserProfile
+);
